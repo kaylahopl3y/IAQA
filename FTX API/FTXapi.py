a920d77c20e64fb4a0ed485c45a5f75307b531cf
Kayla Hopley <69542935+kaylahopl3y@users.noreply.github.com>
2021-06-21 20:07:13 +0200

    Add files via upload

---
+++ FTX API/FTXapi.py
@@ -0,0 +1,15 @@
+import pandas as pd
+import requests
+
+markets = requests.get('https://ftx.com/api/markets').json()
+df = pd.DataFrame(markets['result'])
+df.set_index('name', inplace = True)
+df.T
+
+df.to_csv (r'export_dataframe.csv', index = False, header=True)
+
+markets = pd.DataFrame(requests.get('https://ftx.com/api/markets/BTC-0924').json())
+markets = markets.drop(['success'], axis=1)
+markets.to_csv (r'btcexport_dataframe.csv', index = False, header=True)
+
+
